d91ac31ecbed63aab33a0fbc4ee15f89fd7733f9
ytolosa <49159469+ytolosa@users.noreply.github.com>
2024-05-25 17:14:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 def hello_world():
     """Example Hello World route."""
     name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    return f"Hello {name}! Version 2!!!"
 
 
 if __name__ == "__main__":
